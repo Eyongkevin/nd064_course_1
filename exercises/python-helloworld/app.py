5924792d3d530c8318286e464773e4f7df6ad5a9
Eyong Kevin <tonyparkerkenz@gmail.com>
2021-02-03 21:12:43 +0100

    Feats: Added enpoints
    Added endpoints; 'status' and 'metrics'

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/status')
+def status():
+    data = {"result": "Ok - healthy"}
+    return make_response(jsonify(data), 200)
+
+@app.route('/metrics')
+def metric():
+    data = {"UserCount": 140, "UserCountActive": 23}
+    return make_response(jsonify(data), 200)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
